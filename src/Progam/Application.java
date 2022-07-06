f8078150db0acb264caee59cfb5d1116b08c5937
Alacio Araujo <aleciosilveira97@gmail.com>
2022-07-06 16:23:43 -0300

    seller entity class

--- src/Progam/Application.java
+++ src/Progam/Application.java
@@ -1,13 +1,21 @@
 package Progam;
 
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.seller;
 
 public class Application {
 
 	public static void main(String[] args) {
 	
 		Department obj = new  Department();
-		System.out.println(obj);
+		
+		
+	  seller  seller = new seller (21, "bob", "bob@gmail.com", new Date(), 3000.0, obj );
+	  
+	  System.out.println(seller);
 
 	}
 
