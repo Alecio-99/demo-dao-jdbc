a9f4390812d8c8cbe3c8c7066f8b26bf3a2c9639
Alacio Araujo <aleciosilveira97@gmail.com>
2022-07-06 15:46:30 -0300

    Department entity class

---
+++ src/Progam/Application.java
@@ -0,0 +1,14 @@
+package Progam;
+
+import model.entities.Department;
+
+public class Application {
+
+	public static void main(String[] args) {
+	
+		Department obj = new  Department();
+		System.out.println(obj);
+
+	}
+
+}
--- src/db/DbException.java
+++ src/db/DbException.java
@@ -6,4 +6,4 @@ public class DbException extends RuntimeException {
 	public DbException(String msg) {
 		super(msg);
 	}
-}jjjjjjjjjjjjjjjjjj
+}
